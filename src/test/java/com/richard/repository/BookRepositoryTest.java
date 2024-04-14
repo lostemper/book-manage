bf278dcab83bb5dd734dc408e277439b2991291b
richard <lostemper@gmail.com>
2024-04-14 16:09:48 +0800

    BookRepositoryTest.testGetAllBooks tests passed

--- src/test/java/com/richard/repository/BookRepositoryTest.java
+++ src/test/java/com/richard/repository/BookRepositoryTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
 import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 @DataJpaTest
@@ -48,5 +49,26 @@ public class BookRepositoryTest {
     bookRepository.deleteById(book.getId());
     Optional<Book> deletedBook = bookRepository.findById(book.getId());
     assertFalse(deletedBook.isPresent());
+  }
+
+  @Test
+  public void testGetAllBooks() {
+    // Save some test books
+    Book book1 = new Book();
+    book1.setTitle("book1");
+    book1.setAuthor("author1");
+    book1.setIsbn("1234567890123");
+    book1.setYear(2024);
+    Book book2 = new Book();
+    book2.setTitle("book2");
+    book2.setAuthor("author2");
+    book2.setIsbn("1234567890789");
+    book2.setYear(2024);
+    bookRepository.save(book1);
+    bookRepository.save(book2);
+    // Retrieve all books from the repository
+    List<Book> books = bookRepository.findAll();
+    // Check if the number of retrieved books matches the number of saved books
+    assertEquals(2, books.size());
   }
 }
