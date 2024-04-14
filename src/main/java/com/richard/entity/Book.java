c0be076ae475d044311a3763c1dd1c4e46198206
richard <lostemper@gmail.com>
2024-04-15 02:18:33 +0800

    strategy change to GenerationType.IDENTITY

--- src/main/java/com/richard/entity/Book.java
+++ src/main/java/com/richard/entity/Book.java
@@ -5,7 +5,7 @@ import javax.persistence.*;
 public class Book {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   private String title;
